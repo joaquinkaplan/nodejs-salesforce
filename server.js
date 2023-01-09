926b65e38e9c7fe63ee567f6daf75f3cd124e4d7
Joaquin Kaplan <joaqkaplan@gmail.com>
2023-01-09 12:56:08 +0200

    final push

--- server.js
+++ server.js
@@ -5,19 +5,39 @@ const app = express();
 
 const fs = require("fs");
 const path = require("path");
+const jsforce = require("jsforce");
 const sessions = fs.readFileSync(path.join(__dirname, "sessions.json"));
 
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const { PORT, SF_LOGIN_URL, SF_USERNAME, SF_PASSWORD, SF_TOKEN } = process.env;
+const conn = new jsforce.Connection({ loginUrl: SF_LOGIN_URL });
+
+conn.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, (err, userInfo) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("User Id: " + userInfo.id);
+    console.log("Org Id: " + userInfo.organizationId);
+  }
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello salesforce World");
+  conn.query("SELECT Id, Name FROM Account", (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("Total records" + result.totalSize);
+      // console.log(result.records);
+      // res.send("Hello salesforce World");
+      res.json(result.records);
+    }
+  });
 });
 
 app.get("/api/sessions", (req, res) => {
   res.json(JSON.parse(sessions));
 });
 
-app.listen(port, () => {
-  console.log(`Express server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Express server running on http://localhost:${PORT}`);
 });
